ed4ade50bc9beec3f6fcdd9a1d74dbd3008ed353
Supachai Ghudkam <56030342@kmitl.ac.th>
2017-05-04 23:13:28 +0700

    update
    update

---
+++ LDR.ino
@@ -0,0 +1,16 @@
+int sensorPin = A0;
+int sensorValue = 0;  
+void setup() {
+  pinMode(12, OUTPUT); 
+  Serial.begin(9600); 
+}
+void loop() {
+  sensorValue = analogRead(sensorPin);    
+  Serial.println(sensorValue); 
+  if(sensorValue < 50) 
+ digitalWrite(12,HIGH); 
+ else digitalWrite(12,LOW); 
+ delay(100);                  
+}
+
+
